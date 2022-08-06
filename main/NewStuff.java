f859b4f0c6b08469994ceaccc9f423e3c7b5ee74
NazKovalchuk <enormous048.gmail.com>
2022-08-06 23:58:57 +0300

    Random method feature

---
+++ main/NewStuff.java
@@ -0,0 +1,10 @@
+public class NewStuff {
+    public static void main(String[] args) {
+
+        int min = 9;
+        int max = 40;
+        int r = (int) (Math.random() * (max - min + 1) + min );
+
+
+    }
+}
